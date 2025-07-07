1482f58dc59963175fddcb564db1d97b13e15773
HCGB167 <hunterauten65@gmail.com>
2025-07-07 02:41:19 -0400

    Update secretTextfile.py

--- library/secretTextfile.py
+++ library/secretTextfile.py
@@ -1,7 +1,7 @@
 # This is a secret test file, don't let other people see it.
 # Also, you may have to change some variables before the bot works.
 
-BOT_TOKEN = "zzzzzzzzzzzzzzzzzzzzzzzzzzzz" # Your discord bot token
+BOT_TOKEN = "MTM5MTY2OTkzODQyMjU0NjQ3Mg.GbhB6v.hTBH-kPaRyRSF0aBEuzAZIghCELiGu92qTShH0" # Your discord bot token
 OWNER_ID = 000000000000000 # Your discord user id
 SALT_STR = "noRagrets" # Can be any string you want, you can even leave it empty.
 PREFERRED_TIMEZONE = "GMT" # Make sure it's a valid timezone, see https://gist.github.com/heyalexej/8bf688fd67d7199be4a1682b3eec7568 for more details.
